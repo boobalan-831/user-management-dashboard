8b7b33ed61c89331ba69dff81b5499c77a579f6a
boobalan-831 <boobalan8311@gmail.com>
2025-06-01 12:36:38 +0530

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
-import { Toaster } from "@/components/UI/toaster";
-import { Toaster as Sonner } from "@/components/UI/sonner";
-import { TooltipProvider } from "@/components/UI/tooltip";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
@@ -9,7 +9,7 @@ import Index from "./pages/Index";
 import DashboardPage from "./pages/dashboard/index";
 import AddUserPage from "./pages/dashboard/add";
 import NotFound from "./pages/NotFound";
-import Navbar from "./components/UI/Navbar";
+import Navbar from "./components/ui/Navbar";
 
 const queryClient = new QueryClient();
 
